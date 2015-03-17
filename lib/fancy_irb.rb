9a20c90cf79761bfc1b36c7f401659aee13f1253
Jan Lelis <mail@janlelis.de>
2015-03-17 12:25:46 +0100

    move options out of implementation

--- lib/fancy_irb.rb
+++ lib/fancy_irb.rb
@@ -7,6 +7,42 @@ require_relative 'fancy_irb/terminal_info'
 require_relative 'fancy_irb/implementation'
 
 module FancyIrb
+  DEFAULT_RESULT_PROC = proc{ |context|
+    if context.inspect?
+      context.last_value.inspect
+    else
+      context.last_value
+    end
+  }
+
+  DEFAULT_COLORIZER_PROC = proc{ |value|
+    FancyIrb.real_lengths[:output] =    value.size
+    if defined?(Wirb) && FancyIrb[:colorize, :output]
+      Wirb.colorize_result value
+    else
+      value
+    end
+  }
+
+  DEFAULT_OPTIONS = {
+    :rocket_mode     => true,   # activate or deactivate #=> rocket output
+    :rocket_prompt   => '#=> ', # prompt to use for the rocket
+    :result_prompt   => '=> ',  # prompt to use for normal output
+    :colorize => {              # colors hash. Set to nil to deactivate colorizing
+      :rocket_prompt => [:blue],
+      :result_prompt => [:blue],
+      :input_prompt  => nil,
+      :irb_errors    => [:red],
+      :stderr        => [:red, :bright],
+      :stdout        => nil,
+      :input         => nil,
+      :output        => true, # wirb's output colorization
+     },
+    :result_proc     => DEFAULT_RESULT_PROC,       # how to get the output result
+    :output_procs    => [DEFAULT_COLORIZER_PROC],  # you can modify/enhance/log your output
+    :east_asian_width => false, # set to true if you have double-width characters (slower)
+  }
+
   DEACTIVATE_ROCKET = %w[
     system
     spawn
--- lib/fancy_irb/implementation.rb
+++ lib/fancy_irb/implementation.rb
@@ -16,53 +16,15 @@ class << FancyIrb
   attr_accessor :skip_next_rocket
 
   def start(user_options = {})
-    # track some irb stuff
     @height_counter   = []
     @real_lengths     = { :output => 1, :input_prompt => 9999 } # or whatever
     @stdout_colorful  = false
     @continue         = false
     @skip_next_rocket = false
+    @options = FancyIrb::DEFAULT_OPTIONS.dup
+    @options[:colorize] = @options[:colorize].dup if @options[:colorize]
 
-    # set defaults and parse user options
-    default_result_proc = proc{ |context|
-      if context.inspect?
-        context.last_value.inspect
-      else
-        context.last_value
-      end
-    }
-
-    default_colorizer_proc = proc{ |value|
-      FancyIrb.real_lengths[:output] =    value.size
-      if defined?(Wirb) && FancyIrb[:colorize, :output]
-        Wirb.colorize_result value
-      else
-        value
-      end
-    }
-
-    default_options = {
-      :rocket_mode     => true,   # activate or deactivate #=> rocket output
-      :rocket_prompt   => '#=> ', # prompt to use for the rocket
-      :result_prompt   => '=> ',  # prompt to use for normal output
-      :colorize => {              # colors hash. Set to nil to deactivate colorizing
-        :rocket_prompt => [:blue],
-        :result_prompt => [:blue],
-        :input_prompt  => nil,
-        :irb_errors    => [:red],
-        :stderr        => [:red, :bright],
-        :stdout        => nil,
-        :input         => nil,
-        :output        => true, # wirb's output colorization
-       },
-      :result_proc     => default_result_proc,       # how to get the output result
-      :output_procs    => [default_colorizer_proc],  # you can modify/enhance/log your output
-      :east_asian_width => false, # set to true if you have double-width characters (slower)
-    }
-
-    @options = default_options
-
-    default_options.each{ |key, value|
+    FancyIrb::DEFAULT_OPTIONS.each{ |key, value|
       # (ugly) 1 level deep merge, maybe refactor
       if key == :colorize
         if user_options.has_key?(:colorize) && user_options[:colorize].nil?
@@ -76,7 +38,7 @@ class << FancyIrb
         end
       else
         @options[key] =
-            user_options.has_key?(key) ? user_options[key] : default_options[key]
+            user_options.has_key?(key) ? user_options[key] : FancyIrb::DEFAULT_OPTIONS[key]
       end
     }
 
