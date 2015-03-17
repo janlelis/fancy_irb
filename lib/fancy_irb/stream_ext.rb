362778bc76156e26a4249034e72ef86b70064192
Jan Lelis <mail@janlelis.de>
2015-03-17 20:18:44 +0100

    neither use alias method chain for stdin

--- lib/fancy_irb/stream_ext.rb
+++ lib/fancy_irb/stream_ext.rb
@@ -16,14 +16,10 @@ end
 
 # TODO testing and improving, e.g. getc does not contain "\n"
 class << $stdin
-  FancyIrb::STDIN_HOOKS.each{ |m|
-    msym   = m.to_sym
-    malias = (m+'_non_fancy').to_sym
-
-    if $stdin.respond_to? msym
-      alias_method malias, msym
-      define_method msym do |*args|
-        res = send malias, *args
+  FancyIrb::STDIN_HOOKS.map(&:to_sym).each{ |m|
+    if $stdin.respond_to? m
+      define_method m do |*args|
+        res = super(*args)
         FancyIrb.track_height res
         res
       end
